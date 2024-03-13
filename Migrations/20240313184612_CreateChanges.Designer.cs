4dece56d213d068befdcaef190245349244736df
casmirandrei <casmirandrei@yahoo.com>
2024-03-13 21:11:58 +0200

    Validation and css

--- Migrations/20240313184612_CreateChanges.Designer.cs
+++ Migrations/20240313184612_CreateChanges.Designer.cs
@@ -4,6 +4,7 @@ using AlphaGym.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlphaGym.Migrations
 {
     [DbContext(typeof(GymContext))]
-    partial class GymContextModelSnapshot : ModelSnapshot
+    [Migration("20240313184612_CreateChanges")]
+    partial class CreateChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Member.cs
+++ Models/Member.cs
@@ -11,20 +11,22 @@ namespace AlphaGym.Models
         [StringLength(100)]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Phone number is required")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must be 10 digits")]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
         [Required]
         [Display(Name = "Monthly Subscription")]
         public int MonthlySubscription { get; set; }
+
         [Required]
         [Display(Name = "Subscription Start Date")]
         [DataType(DataType.Date)]
-       // [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+        // [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+         public DateTime SubscriptionStartDate { get; set; }
 
-        public DateTime SubscriptionStartDate { get; set; }
-
+        [Required]
         [Display(Name = "Subscription Expiry Date")]
         [DataType(DataType.Date)]
         //[DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
